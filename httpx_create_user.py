16c92937c18f3f6a724096d9f67f9e424375d453
rgn2836 <bublik0706@gmail.com>
2026-02-11 20:22:14 +0300

    тест отправки get, delete методов

---
+++ httpx_create_user.py
@@ -0,0 +1,38 @@
+import httpx
+from tools.fakers import get_random_email
+
+email = get_random_email()
+password = "pass"
+
+payload = {
+  "email": email,
+  "password": password,
+  "lastName": "string",
+  "firstName": "string",
+  "middleName": "string"
+}
+
+response = httpx.post(url="http://localhost:8000/api/v1/users", json=payload)
+
+
+print(response.status_code)
+print(response.json())
+create_user_response_data = response.json()
+
+payload_auth = {
+  "email": email,
+  "password": password
+}
+
+response = httpx.post(url="http://localhost:8000/api/v1/authentication/login", json=payload_auth)
+print(response.json())
+login_user_response_data = response.json()
+
+headers = {
+    "Authorization": f'Bearer {login_user_response_data["token"]["accessToken"]}'
+    }
+response = httpx.get(
+    url=f"http://localhost:8000/api/v1/users/{create_user_response_data["user"]["id"]}", 
+    headers=headers)
+print(response.json())
+print(response.status_code)
