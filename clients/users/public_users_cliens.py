ecd3af358523b6efa826a76c92d23f2993cd3429
rgn2836 <bublik0706@gmail.com>
2026-02-13 21:06:19 +0300

    Написан API клиент PublicUserClient для работы с /api/v1/users

---
+++ clients/users/public_users_cliens.py
@@ -0,0 +1,27 @@
+from clients.api_client import APIClinet
+
+from httpx import Response
+from typing import TypedDict
+
+class CreateUsersRequest(TypedDict):
+  """
+    Описание структуры запроса на создание пользователя.
+    """
+  email = str
+  password = str
+  lastName = str
+  firstName = str
+  middleName = str
+
+class PublicUserClient(APIClinet):
+    """
+    Клиент для работы с /api/v1/users
+    """
+    def create_user_api(self, request: CreateUsersRequest) -> Response:
+        """
+        Метод создает пользователя.
+
+        :param request: Словарь с email, password, lastname, firstname, middlename.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        APIClinet.post("/api/v1/users", json=request)
