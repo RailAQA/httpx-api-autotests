f5cfa37a44ed6ae0014522c9556ac4c252829b49
rgn2836 <bublik0706@gmail.com>
2026-02-13 21:28:10 +0300

    Написан API клиент UpdateUserRequest для работы с /api/v1/users

---
+++ clients/users/private_users_cliend.py
@@ -0,0 +1,55 @@
+from httpx import Response
+
+from clients.api_client import APIClinet
+from typing import TypedDict
+
+
+class UpdateUserRequest(TypedDict):
+    """
+    Описание структуры запроса на обновление пользователя.
+    """
+    email = str | None
+    lastName = str | None
+    firstName = str | None
+    middleName = str | None
+
+class PrivateUserClient(APIClinet):
+    """
+    Клиент для работы с /api/v1/users
+    """
+
+    def get_user_me_api(self) -> Response:
+        """
+        Метод получения текущего пользователя.
+
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        APIClinet.get("/api/v1/users/me")
+
+    def get_user_api(self, user_id: str) -> Response:
+        """
+        Метод получения пользователя по идентификатору.
+
+        :param user_id: Идентификатор пользователя.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        APIClinet.get(f"/api/v1/users/{user_id}")
+
+    def update_user_api(self, user_id: str, request: UpdateUserRequest) -> Response:
+        """
+        Метод обновления пользователя по идентификатору.
+
+        :param user_id: Идентификатор пользователя.
+        :param request: Словарь с email, lastName, firstName, middleName.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        APIClinet.patch(url=f"/api/v1/users/{user_id}", json=request)
+
+    def delete_user_api(self, user_id: str) -> Response:
+        """
+        Метод удаления пользователя по идентификатору.
+
+        :param user_id: Идентификатор пользователя.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        APIClinet.delete(url=f"/api/v1/users/{user_id}")
