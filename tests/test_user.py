26dc3883bd695548d0ee9324aa3ee43e38d5a934
rgn2836 <bublik0706@gmail.com>
2026-02-25 19:55:14 +0300

    Написан тест на создание пользователя

---
+++ tests/test_user.py
@@ -0,0 +1,21 @@
+from clients.users.public_users_cliens import get_public_users_client
+from clients.users.users_schema import CreateUsersRequestSchema, CreateUserResponseSchema
+from tools.assertions.base import assert_status_code
+from tools.assertions.schema import validate_json_schema
+
+from http import HTTPStatus
+
+from tools.assertions.users import assert_create_user_response
+
+
+def test_create_user():
+    public_users_client = get_public_users_client()
+
+    request = CreateUsersRequestSchema()
+    response = public_users_client.create_user_api(request=request)
+    response_data = CreateUserResponseSchema.model_validate_json(response.text)
+
+    assert_status_code(actual=response.status_code, expected=HTTPStatus.OK)
+    assert_create_user_response(request=request, response=response_data)
+
+    validate_json_schema(instance=response.json(), schema=response_data.model_json_schema())
